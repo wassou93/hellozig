234b6f22587d1be407fbe02720d040bd9b0eb8ca
Wassim Akkari <was.akkari@gmail.com>
2024-10-16 15:10:12 +0200

    examples how to handle optionals, error unions, custom unions

--- src/main.zig
+++ src/main.zig
@@ -1,18 +1,42 @@
 const std = @import("std");
 
+const u8_u16 = union {
+    var1: u8,
+    var2: u16,
+};
+
 pub fn main() void {
-    const _stdout = std.io.getStdOut().writer();
-    _stdout.print("stdout\n", .{}) catch return;
-    std.debug.print("stderr\n", .{});
+    // Optionals
+    var optional_value: ?[]const u8 = null;
+    std.debug.assert(optional_value == null);
+    std.debug.print("\noptional 1\ntype: {}\nvalue: {?s}\n", .{ @TypeOf(optional_value), optional_value });
+    optional_value = "hi";
+    std.debug.assert(optional_value != null);
+    std.debug.print("\noptional 1\ntype: {}\nvalue: {?s}\n", .{ @TypeOf(optional_value), optional_value });
 
-    const arr: [8]u64 = .{ 1, 2, 3, 4, 5, 6, 7, 8 };
+    // Error unions
+    var number_or_error: anyerror!i32 = error.ArgNotFound;
 
-    var bw = std.io.bufferedWriter(_stdout);
-    const stdout = bw.writer();
+    std.debug.print("\nerror union 1\ntype: {}\nvalue: {!}\n", .{
+        @TypeOf(number_or_error),
+        number_or_error,
+    });
 
-    for (0..arr.len) |index| {
-        stdout.print("arr[{}] = {};\n", .{ index, arr[index] }) catch return;
-    }
-    stdout.print("\n", .{}) catch return;
-    bw.flush() catch return;
+    number_or_error = 1234;
+
+    std.debug.print("\nerror union 1\ntype: {}\nvalue: {!}\n", .{
+        @TypeOf(number_or_error),
+        number_or_error,
+    });
+
+    var u8_or_u16: u8_u16 = .{ .var1 = 12 };
+    std.debug.print("\nu8_or_u16 type is {} and value is {!}\n", .{ @TypeOf(u8_or_u16), u8_or_u16 });
+    u8_or_u16 = .{ .var2 = 1200 };
+    std.debug.print("\nu8_or_u16 type is {} and value is {!}\n", .{ @TypeOf(u8_or_u16), u8_or_u16 });
+
+    std.debug.print("\nthis -> {s}\n\n", .{do_something() catch {}});
+}
+
+fn do_something() ![]const u8 {
+    return "[]u8 returned from do_something()";
 }
