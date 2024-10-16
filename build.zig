17f34e696e3dbe8c355188e6379b287c34a496e5
Wassim Akkari <was.akkari@gmail.com>
2024-10-16 11:59:05 +0200

    Implement different ways of printout in zig, (stdout, stderr, buffered printing)

--- build.zig
+++ build.zig
@@ -15,10 +15,16 @@ pub fn build(b: *std.Build) void {
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
 
+    // Check for additional command line arguments
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
 
+    // Create a step named "run" for executing the program
+    const run_step = b.step("run", "Run the program");
+    run_step.dependOn(&run_cmd.step); // Make run_step depend on the run_cmd step
+
+    // Add unit tests
     const exe_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
@@ -27,9 +33,7 @@ pub fn build(b: *std.Build) void {
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
+    // Create a "test" step for running unit tests
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_exe_unit_tests.step);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,22 +1,18 @@
 const std = @import("std");
 
-fn Tensor(comptime Scalar: type) type {
-    return struct {
-        buffer: []Scalar,
+pub fn main() void {
+    const _stdout = std.io.getStdOut().writer();
+    _stdout.print("stdout\n", .{}) catch return;
+    std.debug.print("stderr\n", .{});
 
-        fn init(allocator: std.mem.Allocator, size: usize) std.mem.Allocator.Error!@This() {
-            return .{
-                .buffer = try allocator.alloc(Scalar, size),
-            };
-        }
-    };
-}
+    const arr: [8]u64 = .{ 1, 2, 3, 4, 5, 6, 7, 8 };
 
-pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
+    var bw = std.io.bufferedWriter(_stdout);
+    const stdout = bw.writer();
 
-    const a = try Tensor(f64).init(allocator, 10);
-
-    std.debug.print("{}", .{a});
+    for (0..arr.len) |index| {
+        stdout.print("arr[{}] = {};\n", .{ index, arr[index] }) catch return;
+    }
+    stdout.print("\n", .{}) catch return;
+    bw.flush() catch return;
 }
