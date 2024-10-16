6c793bb467f5e42949ce6d484bab0054d7e224c6
Wassim Akkari <was.akkari@gmail.com>
2024-10-16 08:08:12 +0200

    add tests in build.zig and add comptime example with allocator

--- build.zig
+++ build.zig
@@ -19,6 +19,17 @@ pub fn build(b: *std.Build) void {
         run_cmd.addArgs(args);
     }
 
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
+    const exe_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+
+    // Similar to creating the run step earlier, this exposes a `test` step to
+    // the `zig build --help` menu, providing a way for the user to request
+    // running the unit tests.
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_exe_unit_tests.step);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,22 @@
 const std = @import("std");
 
-pub fn main() void {}
+fn Tensor(comptime Scalar: type) type {
+    return struct {
+        buffer: []Scalar,
+
+        fn init(allocator: std.mem.Allocator, size: usize) std.mem.Allocator.Error!@This() {
+            return .{
+                .buffer = try allocator.alloc(Scalar, size),
+            };
+        }
+    };
+}
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    const a = try Tensor(f64).init(allocator, 10);
+
+    std.debug.print("{}", .{a});
+}
